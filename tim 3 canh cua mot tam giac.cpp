038dd36df8647e615f009578d436f6455a3da7ed
PhamQuynh0408 <54654952+PhamQuynh0408@users.noreply.github.com>
2019-09-06 13:30:26 +0700

    bai so 4

---
+++ tim 3 canh cua mot tam giac.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    
+    while(a,b,c>0){
+    	printf("hay nhap ba canh cua mot tam giac");
+    	 scanf("%d",&a);
+         scanf("%d",&b);
+         scanf("%d",&c);
+        if(a>b+c && b>a+c && c>a+b){
+         printf("la ba canh cua mot tam giac");
+        
+	}}
+	printf("ba canh khong tao thanh mot tam giac");
+	return 0;
+}
