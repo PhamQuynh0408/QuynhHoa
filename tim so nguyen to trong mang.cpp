941147ea3acae78a77843499c940e90fab0662d9
PhamQuynh0408 <54654952+PhamQuynh0408@users.noreply.github.com>
2019-09-10 13:32:04 +0700

    on tap 2

---
+++ tim so nguyen to trong mang.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int ary[20];
+	 
+	for(int a=1; i<20 ; a++ ){
+		int count=0;
+		
+		if(ary[i]%a==0){
+			count++;
+		}
+		esle{
+		a++;}
+		if(count>2){
+	    	printf("khong phai la so nguyen to");
+		}else{
+			printf("la so nguyen to");
+		}
+	}
+	return 0;
+}
